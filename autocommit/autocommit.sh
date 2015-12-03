94bd8dbf5718942308653a23c3e65096c1f10f61
Ron Egli <ron@r-egli.com>
2015-12-02 18:03:49 -0700

    auto-1449104629

---
+++ autocommit/autocommit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Variables
+timestamp=$(date +%s)
+
+cd /your/script/location
+git add *
+
+git status | grep deleted | while read -r line ; do
+    output=$(echo $line | awk {'print $2'})
+    git rm $output
+done
+
+git commit -m "auto-$timestamp"
+git push -u origin master
+
---
+++ autocommit/readme.md
@@ -0,0 +1,3 @@
+This is a simple script that can run on a cron to incrementally push to git repositories 
+over time on a schedule instead of manually. This script also removes any previously 
+removed local files to clean up the repo.
