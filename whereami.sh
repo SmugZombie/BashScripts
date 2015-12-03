d470783623d936086ecc82015e2d296e16fff6c2
Ron Egli <ron@r-egli.com>
2015-12-02 17:59:28 -0700

    NewPush

--- whereami.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-externalIP=$(curl -s http://digdns.com/ip/?mode=quick)
-IPprovider=$(curl -s http://digdns.com/ip/whois.php?ip=$externalIP)
-provider=$(echo $IPprovider | awk -F "\"" {'print $8'})
-echo External IP: $externalIP;
-echo Provider: $provider;
