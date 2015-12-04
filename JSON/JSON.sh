005adfbebca06cb631bc7e236c1f7e71218cbadb
Ron Egli <ron@r-egli.com>
2015-12-03 17:09:30 -0700

    auto-1449187770

---
+++ JSON/JSON.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# User passes in both JSON string and what index they want the value for
+
+echo $1 | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w $2 | awk -F "|" {'print $2'}
