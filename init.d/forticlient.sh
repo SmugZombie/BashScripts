5aeea3e6669256ae48f62db64926e01bbd916963
Ron Egli <ron@r-egli.com>
2016-03-30 09:51:42 -0700

    Fixed Duplicate Paste / Fixed line 50

--- init.d/forticlient.sh
+++ init.d/forticlient.sh
@@ -47,62 +47,7 @@ case "$input" in
 	status)
 		getStatus
 	;;
-	*))
-		echo "Usage: (start|stop|restart|status)"
-	;;
-esac
-root@SmugDesk-Nix:/opt/tvscripts/bin# mv killvpn.sh forticlient.sh
-root@SmugDesk-Nix:/opt/tvscripts/bin# cat forticlient.sh 
-#!/bin/bash
-
-
-function startVPN {
-	echo "Wee"
-}
-
-function stopVPN {
-	killall forticlientsslvpn_cli
-}
-
-function getStatus {
-	processes=$(ps aux | grep forticlientsslvpn_cli | grep -v grep)
-
-	if [ -z "$processes" ]; then
-		echo "Forticlient is NOT Running"
-	else
-		currentIp=$(ifconfig | grep 10.0.0 | awk -F ":" {'print $2'} | awk {'print $1'})
-		echo "Forticlient is Running"
-		echo "Current VPN IP: $currentIp"
-	fi
-}
-
-input=$1
-
-if [ "$input" == "" ]; then
-	while [ "$input" == "" ]; do
-	echo  "Which task do you wish to perform? (start|stop|restart|status)"
-	echo -n ">> "
-	read input
-	done
-fi
-
-
-case "$input" in
-	start)
-		echo "Hello"
-	;;
-	stop)
-		killall forticlientsslvpn_cli
-	;;
-	restart)
-		stopVpn
-		sleep 2
-		startVPN
-	;;
-	status)
-		getStatus
-	;;
-	*))
+	*)
 		echo "Usage: (start|stop|restart|status)"
 	;;
 esac
